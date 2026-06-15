e12dd55b5f6640d2241e8a0ebe3fc476f3d06492
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-15 08:56:18 +0545

    First day

---
+++ day1/day1.py
@@ -0,0 +1,24 @@
+_= "hello! "
+
+
+print(_*3)
+
+#case (types of format of text)
+
+
+#snake case
+my_first_variable="First"
+
+#camel case
+
+mySecondVariable="Second"
+
+#pascal case
+
+MyThirdVariable="Third"
+
+# for comment use Ctrl + / or # for multiline comments
+
+
+#datatype
+#typecasting
