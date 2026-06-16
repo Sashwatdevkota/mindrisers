99cdaade02c3d32e6b293a8436d0469290a73733
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-16 08:11:07 +0545

    day 2

---
+++ day 2/datatype.py
@@ -0,0 +1,9 @@
+#datatypes in python
+# string init float list tuple boolean  etc
+
+
+#string: collection of characters all strings must be inside a ' ' " "
+
+a='hello he\'s' # here \ is a escape character
+
+print(a)
