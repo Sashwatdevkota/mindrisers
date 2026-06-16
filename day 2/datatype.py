2061b7632b595c75cf76d29b2e2c7f8bc49ad91e
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-16 08:32:30 +0545

    work done

--- day 2/datatype.py
+++ day 2/datatype.py
@@ -2,8 +2,73 @@
 # string init float list tuple boolean  etc
 
 
+
+#------------------------STRING-------------------------------------------#
+
 #string: collection of characters all strings must be inside a ' ' " "
 
 a='hello he\'s' # here \ is a escape character
+b="hello \"HE\" is"
+c='''This is a triple quote also used mainly
+    for paragaphs'''
 
-print(a)
+# print(c)
+
+
+#------------------------INTEGER-------------------------------------------#
+
+i=52 # whole numbers
+print(i)
+print(type(i))
+
+
+#------------------------FLOAT-------------------------------------------#
+
+f=15.535 # decimal numbers
+print(f)
+print(type(f))
+
+
+#------------------------Boolean-------------------------------------------#
+
+b=True
+b=False
+print(b)
+print(type(b))
+
+
+#------------------------None-------------------------------------------#
+
+n = None # an empty variable will be declared
+print(n)
+print(type(n))
+
+
+#------------------------Group Datatypes-------------------------------------------#
+# List [] used to define list, multiple data of multiple datatypes, mutable(changeable), ordered
+
+my_list= ['hello',50,55.5,True]
+print(my_list)
+my_list[2]=20
+print(my_list)
+
+#Tuple () used to define list, mutiple data of multiple datatypes, immutable(unchangebale, ordered)
+
+my_tuple= ('hello',50,55.5,True)
+print(my_tuple)
+print(type(my_tuple))
+
+#set: {} used to define set, multiple data of multiple data of multiple datatupes, unordered, does not except duplicate data, mutable
+
+my_set={'hello',50,55.5,True,'hello'}
+print(my_set)
+
+
+#dictionary: {} used to define dictionary, key: value pair ( this can be different datatypes asw), key must be unique
+
+my_dict={1:"ram", 2:"sita", "third": 12}
+print(my_dict)
+
+#keys if duplicated with overright the value 
+my_dict={1:"ram", 2:"sita", "third": 12, 1:"test"}
+print(my_dict)
--- day1/day1.py
+++ day1/day1.py
@@ -1,3 +1,16 @@
+# Types of programming language : Interpreter and compiler 
+
+# Statement: block of code single line statements and multiline statements
+
+# Syntax: Rule, structure, protocol to write code
+
+# Keyword: pre defined words, reserved works
+
+# Variables: user defined, containers to store values
+
+# Conversions of variable: beginning should not start with number or special character except (_)
+
+
 _= "hello! "
 
 
