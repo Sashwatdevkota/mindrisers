e512fa1d7fbb221f8cc085d3c1b30b9b08fe9f5d
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-17 08:25:22 +0545

    logical_operator

---
+++ day 3/logical_operator.py
@@ -0,0 +1,17 @@
+#Logical Operator:
+a=10 
+b=5
+c=5
+
+#and: if both conditions are equal then Output : True, else False
+print(a<b and c ==b)
+
+
+#or : if any one condition is true then output: True, else False
+print(a<b or c==b)
+
+#not: if result is True,outputs False if result is False outputs True
+print(not True)
+
+print(not a<b)
+print(not a<b and c ==b) 
