b6f1eb12bbd184b9bfd8a96cf342006c6c3ab221
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-17 08:45:52 +0545

    Membership

---
+++ day 3/membership.py
@@ -0,0 +1,22 @@
+#Membership operator: checks a data exists in a squential datatype(group datatypes, str)
+#syntax: data operator sequence 
+
+a="Membership"
+
+#in operator: output; True if data exist in the sequence, False otherwise
+
+print("b" in a)
+
+#no in operator: output; False if data exists in the sequence, True otherwise
+print("b" not in a)
+
+
+
+print("For Fruit")
+fruits = ["Apple","Banana","Mango","Orange","Grapes","Pineapple","Papaya","Watermelon","Strawberry","Kiwi"]
+
+# select a data and check if the data exist in the fruit sequence
+
+fruit="Apple"
+
+print(fruit in fruits)
