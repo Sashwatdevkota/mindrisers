7d2ed17e6d1c2fd2d943573b37dcc953604d9581
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-17 08:14:52 +0545

    comparision_operator

---
+++ day 3/comparision_operator.py
@@ -0,0 +1,23 @@
+# Comparision operator: two values compare, Output Boolean(True,False)
+a=10
+b=10
+
+#equal to (==): if two datas are equal output True, else False
+print(a==b)
+
+#not equal to (!=): if two datas are not equal output False, else True
+print(a!=b)
+
+#greater than (>): if a is greater than b then output: True, else False
+
+print(a>b)
+
+#less than (<): if a is less than b then output: True, else False
+print(a<b)
+
+#greater than equal to(>=): if a is greater than equal to b then output: True, else False
+print(a>=b)
+
+#less than equal to (<): if a is less than equal to b then output: True, else False
+print(a<=b)
+
