cc3c795afb2324b94550c8bba696cfeb390b5e0d
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-17 20:25:27 +0545

    assignment day 3

---
+++ day 3/assignment/typecasting.py
@@ -0,0 +1,42 @@
+####    to do:
+
+#Create a list of username:
+#get username from user
+#check if the username exists in the list of usernames
+
+usernames=['Ram',"Hari",'Sita',"Gita","Shyam"]
+username=input("Enter your username :")
+print(username in usernames)
+
+#create a dictionary with username as key and age as value
+#get username from user
+#check if the username exists in the dictionary of username
+
+print("DICT STARTS\n")
+
+usernames={
+  'Ram':30,
+  "Hari":25,
+  'Sita':28,
+  "Gita":19,
+  "Shyam":41
+  }
+
+username=input("Enter your username :")
+print(username in usernames)
+print(usernames[username])
+
+# take data from user aka terminal S
+
+print("Addition from users")
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+print("Sum is",a+b)
+
+
+
+
+# get two numbers from users 
+# / perform arthimetic operations 
+
+#indexing, slicing 
