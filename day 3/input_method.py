6f7fd93d1a444aec7ee2413d2ae87ba74540f9a8
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-17 18:53:01 +0545

    input method and type casting

---
+++ day 3/input_method.py
@@ -0,0 +1,61 @@
+# # a=input("Enter your name :")
+# # print(a)
+# # print(type(a))
+
+# #Type casting: converting datatype
+
+# #convert string to interger: int()
+
+# a=input("Enter first number :")
+# print("for a", a)
+# print(type(a))
+
+# b=int(a)
+# print("for b", b)
+# print(type(b))
+
+# #convert string to float: float()
+
+# c=float(a)
+
+
+# #convert to string: str()
+# d=str(c)
+
+#for list: list()
+fruits={"Apple","Banana","Mango","Orange"}
+print(fruits)
+my_list= list(fruits)
+print(my_list)
+
+
+#for typle: tuple()
+
+my_tuple=tuple(fruits)
+print(my_tuple)
+
+#for set: set()
+
+my_set=set(my_tuple)
+print(my_set)
+
+
+#convert to dictionary: dict() : key:pair()
+a = [('name','ram'),('age',25),("address","KTM")]
+my_dict=dict(a)
+print(my_dict)
+
+####    to do:
+#Create a list of username:
+#get username from user
+#check if the username exists in the list of usernames
+# create a dictionary with username as key and age as value
+#get username from user
+#check if the username exists in the dictionary of username
+
+
+# take data from user aka terminal 
+# get two numbers from users 
+# / perform arthimetic operations 
+
+#indexing, slicing 
