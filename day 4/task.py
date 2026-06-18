6ff0e4eaf6c4bb7ef6c973f98b0660b88945c3d6
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-18 21:40:42 +0545

    task.py

---
+++ day 4/task.py
@@ -0,0 +1,16 @@
+name="ram"
+age = "35"
+
+print("hello", name, "you are", age, "years old.")
+print("hello"+ name+ "you are"+ age+ "years old.") # concatention can be done only on string so when age is int i will show error
+
+print(f"hello {name} you are {age} years old")
+
+#create a list of your hobbies
+
+hobbies = ["Painting", "Hiking", "Chess", "Photography", "Gardening"]
+print(f"I like {hobbies[0]}")
+print(f"I like {hobbies[1]}")
+print(f"I like {hobbies[2]}")
+print(f"I like {hobbies[3]}")
+print(f"I like {hobbies[4]}")
