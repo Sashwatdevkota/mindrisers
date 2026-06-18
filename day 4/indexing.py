4409c26143c191bd5202a4842cb101ce2aa5b217
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-18 08:45:48 +0545

    indexing

---
+++ day 4/indexing.py
@@ -0,0 +1,43 @@
+#Indexing: get or access a single data from sequence, ordered datatype(list, tuple, string, dict)
+
+a = "He llo"
+
+# H = 0
+# e = 1
+# l = 2
+# l = 3
+# o = 4
+
+# print(a[0])
+# print(a[1])
+# print(a[4])
+
+b="I am ( learning)"
+
+#print ( 
+
+print(b[5])
+
+fruits = ["Apple","Banana","Manago","Orange","Grapes","Pineapple","Papaya","Watermelon"]
+#print out orange from list of fruits
+
+print(fruits[3])
+# print(fruits[2])
+# print(fruits[5])
+
+fruits[3]='Cat'
+print(fruits[3])
+
+# dict
+
+my_dict= {"name":"ram", "age":35, "contact":"98041803910", "address":"ktm"}
+
+print(my_dict["name"])
+
+my_dict["name"]="SITA"
+print(my_dict)
+
+
+my_tuple=("Apple","Banana","Carrot","Mango","Pineapple")
+print(my_tuple[1])
+my_tuple[1]="Cat"
