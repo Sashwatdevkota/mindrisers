13a11134fe3575404ad665afa4850eca99aa79f1
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-21 20:59:37 +0545

    task 2

--- day 5/if.py
+++ day 5/if.py
@@ -68,4 +68,34 @@ else:
   print("Please use the selected operator only")  
   
 
+# get user's exam marks
+# if the mark is greater than 100 and less than 0, print a statment
+# if the mark is greater than 90 and less then 100, print a statment("Excekllent")
+# if the mark is greater than 80 and less than 90 , print a statement
+# if the mark is greater than 70 and less than 80 , print a statement
+# if the mark is greater than 60 and less than 70 , print a statement
+# if the mark is greater than 50 and less than 60 , print a statement
+# if the mark is greater than 40 and less than 50 , print a statement
+# if the mark is less than 40, print a statment
 
+
+marks=float(input("Enter your grade"))
+if (marks > 100 or marks <0):
+  print("Please enter a valid mark")
+elif (marks > 90 and marks < 100):
+  print("you got A+")
+elif (marks > 80 and marks <= 90):
+  print("you got A")
+elif (marks > 70 and marks <=80):
+  print("you got B+")
+elif (marks > 60 and marks <= 70):
+  print("you got B")
+elif (marks > 50 and marks <=60):
+  print("you got C+")
+elif (marks > 40 and marks < 50):
+  print("you got C")
+else:
+  print("failed")
+  
+  
+  
