505863e6dbe9b33be5ea73f3d347f04504be5d30
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-23 08:34:42 +0545

    list of dict

--- day 7/forloop.py
+++ day 7/forloop.py
@@ -1,6 +1,5 @@
 # for loop:
 # iterable: sequential data(group data and string data)
-a = [1, 2, 3, 4, 5]
 # iteration: process of moving from first index of data to last index in the interable
 # iterator: variable using to perform iteraation in iterable
 
@@ -10,5 +9,25 @@ a = [1, 2, 3, 4, 5]
 # statment1
 # statment2
 
+a = [1, 2, 3, 4, 5]
+
 for i in a:
-    print("For loop",i)
+    print("For loop", i)
+
+print("\n" * 5)
+
+
+# to do:
+# create a list of hobbies
+# print out a statement using each hobbies
+
+hobbies = ["Coding", "Gym", "Guitar", "Chess", "Football"]
+for hobbie in hobbies:
+    print(hobbie)
+
+
+# create a list of dictionary with name,age,contact
+infos=[{"name":"ram","age":20,"contact":1},{"name":"ram","age":21,"contact":4},{"name":"sita","age":22,"contact":3}]
+
+for info in infos:
+  print(f"Hello my name is {info["name"]}, my age is {info["age"]}, my contact no is {info["contact"]}")
