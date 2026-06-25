5d0b14405f74a256452a59d2c952bdd23c024d50
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-25 08:17:20 +0545

    intro to functions

---
+++ day 7.2/function.py
@@ -0,0 +1,29 @@
+# # functions: like variables, block of code is assigned to a function
+# # specific task
+
+
+# def function_name(var1, var2):
+#     print("hello")
+#     return None
+
+
+# def addition():  # function define
+#     a = int(input("Enter a number"))
+#     b = int(input("Enter a number"))
+#     print(a + b)
+
+
+# addition()  # function call
+
+# # do assignments
+
+
+# def intro():
+#     name = "Ram"
+#     age = 35
+#     print(f"My name is {name} and my age is {age}")
+
+
+# intro()
+
+
