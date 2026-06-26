d965074e14d9bd382af345968d4043ebd518644b
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-26 08:44:29 +0545

    kwargs

---
+++ day 8/kwargs.py
@@ -0,0 +1,18 @@
+# Kwargs: keywords arguments, sent multiple keyword arguments to a parameter
+# to define kwargs use **var, it is followed by kwargs_name
+
+
+# def student(**kwargs):
+#     print(kwargs)
+#     print(kwargs.items()) #value is stored as tuple
+#     for i, j in kwargs.items():
+#         print(f"{i}:{j}")
+
+
+# student(name="20")
+
+
+# function based
+# add two number
+# get a number from a user and multiple it with the sum
+
