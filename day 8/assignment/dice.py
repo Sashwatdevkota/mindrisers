a3bb651a2f51f030267d132bb974e8e689d1ad26
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-28 22:42:11 +0545

    assignment

---
+++ day 8/assignment/dice.py
@@ -0,0 +1,6 @@
+# 7. Suppose we have a pair of dice.
+# First, use the random module to “roll” the two dice.
+# Each die (named die1 and die2) should have an integer value from 1 to 6.
+# Store the sum of the two random values in variable named total.
+# Using a while loop, check if total is 2. If it isn't, print the string 'Nope' and keep "rerolling" the dice.
+# Let the loop run until the total is 2, then print 'Snake eyes!
