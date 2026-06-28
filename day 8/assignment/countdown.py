c245d7e6942c506519ba389892d1b8e48fa50494
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-28 22:42:40 +0545

    questions

---
+++ day 8/assignment/countdown.py
@@ -0,0 +1,4 @@
+# 6. a countdown from 10 to 1.
+# Use a for loop that counts down by using the "step" value in range().
+# Inside the loop, print the numbers from 10 to 1, each on its own line.
+# When the loop finishes the countdown, print this exact string.
