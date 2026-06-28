3517db4f534ffc8cd32350b7a0bc6ff0929edf63
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-28 22:40:15 +0545

    some task works

---
+++ day 8/assignment/seasons.py
@@ -0,0 +1,22 @@
+# 3. Instructions
+# four seasons in the year — winter, spring, summer, or fall
+# Ask the user the month number using the input() function.
+# Check for the four seasons using an if/elif/else statement and logical operators:
+# month is 1, 2, 3, print 'Winter'
+# month is 4, 5, 6, print 'Spring'
+# month is 7, 8, 9, print 'Summer'
+# month is 10, 11, 12, print 'Autumn'
+# Everything else is 'Invalid'
+
+
+month = int(input("Enter a number between 1 and 12"))
+if month < 1 or month > 12:
+    print("Please enter a valid month number")
+elif month == 1 or month == 2 or month == 3:
+    print("Winter")
+elif month == 4 or month == 5 or month == 6:
+    print("Spring")
+elif month == 7 or month == 8 or month == 9:
+    print("Summer")
+elif month == 10 or month == 11 or month == 12:
+    print("Autumn")
