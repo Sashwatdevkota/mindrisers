8509a938c6026e43f0eedfbf6c86fdacf7d530cc
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-30 08:22:03 +0545

    oop second part

--- day 11/oop.py
+++ day 11/oop.py
@@ -20,3 +20,19 @@ class Person:
 r1 = Person()  # r1 is object, Person() is class call
 
 print(r1.name)
+
+
+sita = Person()
+
+sita.name = "Sita"
+sita.age = "30"
+sita.address = "BKT"
+sita.gender = "female"
+
+# sita.abc = "ABC" #adding separate attribute to the object sita
+
+print(sita.name)
+print(sita.age)
+print(sita.address)
+print(sita.gender)
+print(sita.abc)
