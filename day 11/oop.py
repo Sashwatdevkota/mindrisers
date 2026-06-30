21ad1ee5c8a0f68f80de0aaf54882435a7045b4f
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-06-30 08:17:05 +0545

    oop introduction

---
+++ day 11/oop.py
@@ -0,0 +1,22 @@
+# oop: object oriented programming
+# class : structures, blueprint
+# object : data created using class
+
+
+# syntax:
+class class_name:
+    # variables are called attributes
+    # functions are called methods
+    var = 1
+
+
+class Person:
+    name = "Ram"
+    age = 35
+    address = "KTM"
+    gender = "male"
+
+
+r1 = Person()  # r1 is object, Person() is class call
+
+print(r1.name)
